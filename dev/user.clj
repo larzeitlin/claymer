fb1991261a316d80ace5f3b5ac203ed5cd90b71e
luke <larzeitlin@gmail.com>
2026-02-16 21:06:30 +1300

    add additional doc from .md

--- dev/user.clj
+++ dev/user.clj
@@ -6,10 +6,13 @@
 (comment
   (require '[scicloj.clay.v2.api :as clay])
   (clay/make! {:base-source-path "test"
+               :source-path ["claymer/core_test.clj"
+                             "claymer/core_test_additional.md"]
                :render true
                :book {:title "Claymer Documentation"}
                :first-as-index true
                :clean-up-target-dir true
                :remote-repo {:git-url "https://github.com/larzeitlin/claymer"
                              :branch "main"}
-               :format [:quarto :html]}))
+               :format [:quarto :html]
+               :subdirs-to-sync ["test"]}))
